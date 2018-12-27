7a9042548357364046f9ad3710089a16fe41706f
isaac_you <isaac_you@mhand.net>
2018-12-27 12:17:40 +0900

    * tizen.voicecontrol 추가

---
+++ tizen/voicecontrol.d.ts
@@ -0,0 +1,47 @@
+import tizen = require('./index')
+
+export enum VoiceControlResultEvent {
+  SUCCESS = 'SUCCESS',
+  FAILURE ='FAILURE'
+}
+
+export enum VoiceControlCommandType {
+  FOREGROUND = 'FOREGROUND'
+}
+
+declare module './index' {
+  interface TizenStatic {
+    readonly voicecontrol: VoiceControl.VoiceControlClientManager
+  }
+
+  namespace VoiceControl {
+
+    interface VoiceControlClientManager {
+      getVoiceControlClient():VoiceControlClient;
+    }
+
+    interface VoiceControlClient {
+      getCurrentLanguage():DOMString;
+      setCommandList(list:VoiceControlCommand [], type?:VoiceControlCommandType):void;
+      unsetCommandList(type?:VoiceControlCommandType):void
+      addResultListener(listener:VoiceControlResultCallback):long;
+      removeResultListener(id:long):void;
+      addLanguageChangeListener(listener:VoiceControlLanguageChangeCallback):long;
+      removeLanguageChangeListener(id:long):void;
+      release():void;
+    }
+
+    interface VoiceControlCommand {
+      command:DOMString;
+      type:VoiceControlCommandType;
+    }
+
+    interface VoiceControlLanguageChangeCallback {
+      onlanguagechanged (previous:DOMString, current:DOMString):void;
+    }
+
+    interface VoiceControlResultCallback {
+      onresult(event:VoiceControlResultEvent, list:VoiceControlCommand[], results:DOMString):void;
+   }
+  }
+}
