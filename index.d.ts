aecf2870c6443171690c7ba74de30fa09908c2e7
isaac_you <isaac_you@mhand.net>
2019-01-02 16:03:39 +0900

    * main index.d.ts 추가

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import * as WebApis from 'webapis';
+import * as Tizen from 'tizen';
+
+
+declare global {
+  // This contains duplicates of some types in lib.dom.d.ts in order to support typescript 2.0
+  const webapis: WebApis.WebApisStatic;
+  const tizen: Tizen.TizenStatic;
+  interface Window {
+      webapis: WebApis.WebApisStatic;
+      tizen: Tizen.TizenStatic;
+  }
+}
--- webapis/index.d.ts
+++ webapis/index.d.ts
@@ -10,11 +10,3 @@ declare namespace WebApis {
   // tslint:disable-next-line no-empty-interface (This will be augmented)
   interface WebApisStatic {}
 }
-
-declare global {
-  // This contains duplicates of some types in lib.dom.d.ts in order to support typescript 2.0
-  const webapis: WebApis.WebApisStatic;
-  interface Window {
-      webapis: WebApis.WebApisStatic;
-  }
-}
