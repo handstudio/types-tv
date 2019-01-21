d67b2717a95faf64ec76a089ddc676ca00ed94de
Joo <lotus1128@mhand.net>
2019-01-21 13:26:26 +0900

    * tizen.appcontrol 추가

--- tizen/application.ts
+++ tizen/application.ts
@@ -1,6 +1,31 @@
 import {Tizen} from './tizen';
 
 export declare module Application {
+  enum ApplicationControlLaunchMode { 
+    SINGLE, 
+    GROUP 
+  }
+
+  type ApplicationId = Tizen.DOMString;
+
+  interface ApplicationControlData {
+    key: Tizen.DOMString;
+    value: Tizen.DOMString[];
+  }
+
+  interface ApplicationControl {
+    operation: Tizen.DOMString;
+    uri?: Tizen.DOMString;
+    mime?: Tizen.DOMString;
+    category?: Tizen.DOMString;
+    data: ApplicationControlData[];
+    launchMode: ApplicationControlLaunchMode;
+  }
+
+  interface ApplicationControlDataArrayReplyCallback {
+    onsuccess(data?: ApplicationControlData[]): () => void;
+    onfailure: () => void;
+  }
 
   interface Application {
     exit(): void;
@@ -8,6 +33,13 @@ export declare module Application {
   }
   interface ApplicationManager {
     getCurrentApplication(): Application;
+    launchAppControl: (
+      appControl: ApplicationControl,
+      id?: ApplicationId,
+      successCallback?: Tizen.SuccessCallback,
+      errorCallback?: Tizen.ErrorCallback,
+      replyCallback?: ApplicationControlDataArrayReplyCallback
+    ) => void;
     // 나머지 추가 필요
   }
 }
