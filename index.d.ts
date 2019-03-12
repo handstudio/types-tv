8d0798001c6f872276cf71640bfe037b1b14b1b0
kimsy89 <kimsy89@mhand.net>
2019-03-12 14:33:13 +0900

    * modify player name

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,8 @@
 
 import * as webapis from './webapis';
 import * as tizen from './tizen';
-import * as stvplayer from './avplayer';
+import * as avplayer from './avplayer';
 
 export const WebApis: typeof webapis;
 export const Tizen: typeof tizen;
-export const Player: typeof stvplayer;
+export const AVPlayer: typeof avplayer;
