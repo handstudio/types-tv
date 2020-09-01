5c5fe80e637045825402250333841968411ae8d5
sangkooo <psk@mhand.net>
2020-09-01 16:47:54 +0900

    :chore Fix rm dist

--- dist/tizen/websetting.d.ts
+++
@@ -1,7 +0,0 @@
-import {Tizen} from './tizen';
-
-export declare module WebSetting {
-  interface WebSettingManager {
-    setUserAgentString: (userAgent: String, successCallback?: Tizen.SuccessCallback) => void;
-  }
-}
--- dist/webapis/logging.d.ts
+++
@@ -1,8 +0,0 @@
-import { WebApis } from './webapis';
-
-export declare module Logging {
-  interface LoggingManager {
-    setServiceConfigInfo: ( servicename: WebApis.DOMString, queuemax: WebApis.long, expiration: WebApis.long, threshold: WebApis.long, loglevel: WebApis.long,  serverUrl: WebApis.DOMString, onsuccess?: WebApis.SuccessCallback, onerror?: WebApis.ErrorCallback) => void;
-    addEventLog: ( eventName: WebApis.DOMString,  time: WebApis.DOMString,  category: WebApis.DOMString,  value: WebApis.DOMString,  desc: WebApis.DOMString, onsuccess?: WebApis.SuccessCallback, onerror?: WebApis.ErrorCallback) => void;
-  }
-}
