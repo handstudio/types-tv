98e9eedeb92bef5943b0db38f4431afcb7079157
sangkooo <psk@mhand.net>
2020-09-01 15:51:31 +0900

    :chore Remove dist

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="./webapis/index.d.ts"/>
-/// <reference path="./tizen/index.d.ts"/>
-
-import * as webapis from './webapis';
-import * as tizen from './tizen';
-
-export const WebApis: typeof webapis;
-export const Tizen: typeof tizen;
